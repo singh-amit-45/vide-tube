552cdc06cc99a3147fc2bbcf578804d2a511e55a
Amit Singh <amitsingh100014@gmail.com>
2026-04-15 15:51:25 +0530

    remove env file for security

--- src/index.js
+++ src/index.js
@@ -8,6 +8,15 @@ import connectDB from "./db/index.js"
 }) 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000, ()=>{
+        console.log(`SERVER IS RUNNING AT PORT ${process.env.PORT||8000}`)
+    })
+})
+.catch((err)=>{
+    console.log("MANGO DB CONNECTION  FAILED!!!",err);
+})
+
  
 
 
